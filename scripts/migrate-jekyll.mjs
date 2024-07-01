8cf872efba9b462ffcd03ebd30ec68b544366023
Sebastian Beltran <bjohansebas@gmail.com>
2024-07-01 10:52:10 -0500

    add migrate script

---
+++ scripts/migrate-jekyll.mjs
@@ -0,0 +1,52 @@
+import { existsSync, readFileSync } from "node:fs";
+import { mkdir, readdir, writeFile } from "node:fs/promises";
+import path from "node:path";
+
+const OUT_FOLDER = path.join(process.cwd(), "out");
+if (!existsSync(OUT_FOLDER)) {
+  await mkdir(OUT_FOLDER);
+}
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  throw new Error("migrate-jekyll.mjs: requires a least one parameter");
+}
+
+for (const arg of args) {
+  const SCAN_FOLDER = path.join(process.cwd(), arg);
+
+  const INCLUDE_FORMAT = /\{\%\sinclude\s[\w\/\-]+\.\w+(\.\w+)?\s\%\}/;
+
+  try {
+    const files = await readdir(SCAN_FOLDER);
+
+    for (const file of files) {
+      const contentFile = readFileSync(path.join(SCAN_FOLDER, file), "utf8");
+
+      const linesForLine = contentFile.split("\n");
+
+      const newFile = linesForLine
+        .map((line) => {
+          if (INCLUDE_FORMAT.test(line)) {
+            const pathOfFile = line.trim().split(" ")[2];
+
+            const newContentLine = readFileSync(path.join(process.cwd(), pathOfFile), "utf8");
+
+            return newContentLine;
+          }
+          return line;
+        })
+        .join("\n");
+
+      const newName = file.replace(/\.\w{1,4}/, ".mdx");
+
+      console.log(arg);
+      await mkdir(path.join(OUT_FOLDER, arg));
+
+      await writeFile(path.join(OUT_FOLDER, arg, newName), newFile);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
